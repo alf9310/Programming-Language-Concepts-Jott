495c27fc35d6c3d9df04fd82e60c90006d8c6f49
Kaiy Muhammad <kkm4289@rit.edu>
2024-09-07 16:25:11 -0400

    made token loop, whitespaces, and single chars

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -7,16 +7,98 @@ package provided;
  **/
 
 import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class JottTokenizer {
+  enum State {
+    START, WAITINGFORNEWLINE
+  }
 
-	/**
-     * Takes in a filename and tokenizes that file into Tokens
-     * based on the rules of the Jott Language
-     * @param filename the name of the file to tokenize; can be relative or absolute path
-     * @return an ArrayList of Jott Tokens
-     */
-    public static ArrayList<Token> tokenize(String filename){
-		return null;
-	}
+  // @return character array
+  public static ArrayList<Character> process_file(String filename) {
+    ArrayList<Character> chars = new ArrayList<>();
+    // TODO not sure why I have to change the file name. Will ask prof --Kaiy
+    try (FileReader reader = new FileReader("test-cases/" + filename)) {
+      BufferedReader buff_reader = new BufferedReader(reader);
+      int ch = 0;
+      while ((ch = buff_reader.read()) != -1) {
+        chars.add((char) ch);
+        System.out.print((char) ch);
+      }
+    } catch (IOException exception) {
+      exception.printStackTrace();
+      System.err.println("Error processing " + filename);
+    }
+
+    return chars;
+  }
+
+  /**
+   * Takes in a filename and tokenizes that file into Tokens
+   * based on the rules of the Jott Language
+   * 
+   * @param filename the name of the file to tokenize; can be relative or absolute
+   *                 path
+   * @return an ArrayList of Jott Tokens
+   */
+  public static ArrayList<Token> tokenize(String filename) {
+    ArrayList<Character> chars = process_file(filename);
+    ArrayList<Token> tokens = new ArrayList<>();
+
+    int line_num = 1;
+    State state = State.START;
+
+    for (int i = 0; i < chars.size(); i++) {
+      char ch = chars.get(i);
+      switch (state) {
+        case START:
+          // Whitespace
+          if (ch == ' ' || ch == '\t') {
+            continue;
+          } else if (ch == '\n') {
+            line_num++;
+          } else if (ch == '#') {
+            state = State.WAITINGFORNEWLINE;
+          }
+          // All the single character tokens
+          else if (ch == ',') {
+            Token token = new Token(",", filename, line_num, TokenType.COMMA);
+            tokens.add(token);
+          } else if (ch == '[') {
+            Token token = new Token("[", filename, line_num, TokenType.L_BRACKET);
+            tokens.add(token);
+          } else if (ch == ']') {
+            Token token = new Token("]", filename, line_num, TokenType.R_BRACKET);
+            tokens.add(token);
+          } else if (ch == '{') {
+            Token token = new Token("{", filename, line_num, TokenType.L_BRACE);
+            tokens.add(token);
+          } else if (ch == '}') {
+            Token token = new Token("}", filename, line_num, TokenType.R_BRACE);
+            tokens.add(token);
+          } else if (ch == ';') {
+            Token token = new Token(";", filename, line_num, TokenType.SEMICOLON);
+            tokens.add(token);
+          } else if (ch == ':') {
+            Token token = new Token(":", filename, line_num, TokenType.COLON);
+            tokens.add(token);
+          } else if (ch == '=') {
+            Token token = new Token("=", filename, line_num, TokenType.ASSIGN);
+            tokens.add(token);
+          }
+          break;
+
+        case WAITINGFORNEWLINE:
+          if (ch == '\n') {
+            state = State.START;
+          }
+          break;
+      }
+
+    }
+
+    return tokens;
+  }
 }
