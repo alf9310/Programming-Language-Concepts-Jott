aca771f4866e952529528b137051f637cdf8e465
Sejal Abhijit Bhattad <sab2713@rit.edu>
2024-10-07 17:12:56 -0400

    Fixed indentation in FunctionCallNode

--- src/nodes/FunctionCallNode.java
+++ src/nodes/FunctionCallNode.java
@@ -10,27 +10,21 @@ public class FunctionCallNode implements OperandNode {
     ArrayList<ParamNode> params;
 
     public FunctionCallNode(IdNode id, ArrayList <ParamNode> params){   
-	this.id = id;
-this.params = params
-
-
-}
+	    this.id = id;
+        this.params = params
+    }
 
     public static FunctionCallNode parse(ArrayList <Token> tokens) throws Exception{
-       if(tokens.isEmpty()){
-		throw new Exception(“Syntax Error\n[message]\n[filename, linenum from this token]\n”);
-}
-
-
-if(!(tokens.get(0).getTokenType() == TokenType.FC_HEADER)){
+        if(tokens.isEmpty()){
+	        throw new Exception(“Syntax Error\n[message]\n[filename, linenum from this token]\n”);
+        }
+        if(!(tokens.get(0).getTokenType() == TokenType.FC_HEADER)){
             throw new Exception(“Syntax Error\n[message]\n[filename, linenum from this token]\n”);
         }
-	// might want to check length first (in case no index 1 exists)
-	if(!(tokens.get(1).getTokenType() == TokenType.ID_KEYWORD && 
-tokens.get(1).getToken().charAt(0).isLowerCase())) {
+	    // might want to check length first (in case no index 1 exists)
+	    if(!(tokens.get(1).getTokenType() == TokenType.ID_KEYWORD && tokens.get(1).getToken().charAt(0).isLowerCase())) {
 		throw new Exception(“Syntax Error\n[message]\n[filename, linenum from this token]\n”);
-}
-
+        }
 
         Token number = tokens.pop(0);
         return new FunctionCallNode(id, params);
@@ -53,7 +47,6 @@ tokens.get(1).getToken().charAt(0).isLowerCase())) {
         }
 
         return jottString.toString();
-
     }
 
     @Override
