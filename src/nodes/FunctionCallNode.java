8dd31e12960295d14517a06cfbc8e45f60d414b3
km4289 <km4289@rit.edu>
2024-12-03 16:44:42 -0500

    built in functions

--- src/nodes/FunctionCallNode.java
+++ src/nodes/FunctionCallNode.java
@@ -196,13 +196,8 @@ public class FunctionCallNode implements OperandNode, BodyStmtNode {
             return null;
         }
 
-        //Concat
-        // validation checks should already be covered by geneneral validation
-        if (this.id.getToken().getToken().equals("print")) {
-            //TODO not sure what the expression node would be in a concat call
-        }
-
         // ----------General Use-Case----------
+        Object returnValue = null;
         // Save the current scope
         String previousScope = symbolTable.current_scope;
 
@@ -215,11 +210,22 @@ public class FunctionCallNode implements OperandNode, BodyStmtNode {
         // Execute the parameters (handles setting their values in the symbol table)
         this.params.execute(symbolTable);
 
-        // Locate and execute the corresponding FunctionDefNode
-        FuncDefNode functionDefNode = funcInfo.getFunctionDefNode();
-
-        // Execute the function body and capture its return value
-        Object returnValue = functionDefNode.execute(symbolTable);
+        if (this.id.getToken().getToken().equals("concat")) {
+            String str1 = symbolTable.getVar("str1").value;
+            String str2 = symbolTable.getVar("str2").value;
+            returnValue = str1 + str2;
+            
+        }
+        else if (this.id.getToken().getToken().equals("length")) { 
+            String str = symbolTable.getVar("str").value;
+            returnValue = str.length();
+        } 
+        else {
+            // Locate and execute the corresponding FunctionDefNode
+            FuncDefNode functionDefNode = funcInfo.getFunctionDefNode();
+            // Execute the function body and capture its return value
+            returnValue = functionDefNode.execute(symbolTable);
+        }
 
         // Restore the previous scope
         symbolTable.current_scope = previousScope;
